d865dd4033346ae739f39f1daf49aece633b4820
Vera <vera.svistun05@gmail.com>
2020-02-25 13:19:41 +0300

    отредактировала код

--- filter/script.js
+++ filter/script.js
@@ -10,25 +10,14 @@ var MyTable = React.createClass({
 
       getInitialState: function() {
         return { 
-          key1:'resetSort',
-          key2:'resetSearch',
           listH:this.props.list,
           searchValue:'',
-          sortCheked:false,
-          
+          sortChecked:false,
         };
       },
 
-      sortList: function() {
-       
-        if(this.state.sortCheked==true) { 
-          this.setState({sortCheked:false})
-          this.setState({key1:'resetSort'})
-        } else {
-           this.setState({sortCheked:true})
-           this.setState({key1:'chekedSort'})
-        }
-      
+      sortList: function(EO) {
+          this.setState({sortChecked:EO.target.checked})    
       },
 
       searchStr: function(EO) {
@@ -42,20 +31,20 @@ var MyTable = React.createClass({
         });
         this.setState({listH:newList})
         this.setState({searchValue:str});
-        this.setState({key2:'search'});
+        
       },
       reset: function() {
         this.setState({listH:this.props.list});
-        this.setState({key1:'resetSort'})
-        this.setState({key2:'resetSearch'});
         this.setState({searchValue:''});
-        this.setState({sortCheked:false});
+        this.setState({sortChecked:false});
       },
       
     render: function(){
       
        var list= [];
-       if(this.state.sortCheked==false)
+      if(this.state.listH) {
+
+       if(this.state.sortChecked==false)
            {this.state.listH.forEach(function(i) {
          var item=React.DOM.option({key:i}, i)
            list.push(item)
@@ -65,11 +54,11 @@ var MyTable = React.createClass({
                 list.push(item)
                 });
            }
-          
+      }
            
       return React.DOM.div( {className:'MyTable'}, 
-      React.DOM.input( {key:this.state.key1,type:'checkbox', className:'checkbox',defaultChecked:this.state.sortCheked, onClick:this.sortList}),
-      React.DOM.input({key:this.state.key2, type:'text',className:'search', defaultValue:this.state.searchValue, onChange:this.searchStr}), 
+      React.DOM.input( {type:'checkbox', className:'checkbox',checked:this.state.sortChecked, onClick:this.sortList}),
+      React.DOM.input({ type:'text',className:'search', value:this.state.searchValue, onChange:this.searchStr}), 
         React.DOM.input({type:'button',value:"Сброс",className:'reset', onClick:this.reset }), 
         
          React.DOM.form(null,
