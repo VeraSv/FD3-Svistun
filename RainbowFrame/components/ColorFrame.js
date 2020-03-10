9fa04ebaa045031c13567a9b6337786f413a2c24
VeraSv <47688337+VeraSv@users.noreply.github.com>
2020-03-10 09:44:48 +0300

    Add files via upload

---
+++ RainbowFrame/components/ColorFrame.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import RainbowFrame from './RainbowFrame';
+
+class ColorFrame extends React.Component {
+    render() { let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
+    return (
+        <RainbowFrame colors={colors}>
+          Hello!
+        </RainbowFrame>
+      );
+}
+}
+
+export default ColorFrame 
