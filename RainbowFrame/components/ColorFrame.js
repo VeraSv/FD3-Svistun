326c0c564b56590a6abdfc0de2696f6892c4d7d5
Vera <vera.svistun05@gmail.com>
2020-03-10 10:05:25 +0300

    отредактировала код

--- RainbowFrame/components/ColorFrame.js
+++ RainbowFrame/components/ColorFrame.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import RainbowFrame from './RainbowFrame';
+import './ColorFrame.css'
 
 class ColorFrame extends React.Component {
     render() { let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
     return (
         <RainbowFrame colors={colors}>
-          Hello!
+          <div className='Block'>Hello!</div>
         </RainbowFrame>
       );
 }
